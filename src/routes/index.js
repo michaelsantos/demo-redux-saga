97da75e9cd0e4600f5e092e859d204aab057f687
Michael Santos <michael_lsantos@yahoo.com.br>
2019-05-05 17:28:55 -0300

    First commit

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from '../pages/main';
+import Footer from '../components/Footer';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Main} />
+    </Switch>
+
+    <Footer />
+  </BrowserRouter>
+);
+
+export default Routes;
